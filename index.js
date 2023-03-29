95a385dd629885b651b7f9a53abad7c45f1f2e66
Валериан Воротников <valerian7202@gmail.com>
2023-03-29 15:48:46 +0300

    enemies + simple AI + damage

--- index.js
+++ index.js
@@ -8,6 +8,63 @@ class Vector2 {
   toString(){
     return `[${this.x}, ${this.y}]`;
   }
+  minus(b) {
+    return new Vector2(b.x - this.x, b.y - this.y);
+  }
+  get length(){
+  return Math.sqrt(this.x**2 + this.y**2)
+  }
+  normalize() {
+    return new Vector2(this.x / this.length, this.y / this.length);
+  }
+  normalizeRound() {
+    return new Vector2(Math.round(this.x / this.length), Math.round(this.y / this.length));
+  }
+}
+
+class Enemy {
+  health = 50;
+  damage = 10;
+
+  minHealth = 20;
+  maxHealth = 60;
+
+  difficultyMultyplier = 3;
+
+  get isDead() { return this.health <= 0; }
+
+  position = null;
+
+  constructor(position, difficulty = 1) {
+    this.position = position;
+    this.health = randomInRange(this.minHealth, this.maxHealth) + difficulty * this.difficultyMultyplier;
+  }
+
+  receaveDamage(damage) {
+    this.health -= damage;
+  }
+
+  static processPositionsToEnemies(positions) {
+    var ret = [];
+
+    for (const positionIndex in positions) {
+      ret.push(new Enemy(positions[positionIndex]));
+    }
+
+    return ret;
+  }
+
+  
+  static createEnemyMatrix(length, width, enemies) {
+    var ret = Array(length).fill(0).map(x => Array(width).fill(null));
+    for (const enemyIndex in enemies) {
+      let enemy = enemies[enemyIndex];
+      let position = enemy.position;
+      ret[position.x][position.y] = enemy;
+    }
+
+    return ret;
+  }
 }
 
 function Enum(values){
@@ -33,6 +90,7 @@ class Game {
   lookUpTypesInverse = this.lookUpTypes.reduce((obj, cur, i) => ({ ...obj, [cur]: i }), {});
   // 
   fieldClass = 'field';
+  inventoryClass = 'inventory';
   // 
   tileSize = 50; 
   // 
@@ -56,18 +114,35 @@ class Game {
   swordCount = 2;
   healthPotionCount = 10;
   enemyCount = 10;
-
+  // 
+  playerAttackRange = 1;
+  maxPlayerHealth = 100;
+  playerDamage = 20;
+  maxPlayerDamage = 20;
+  // 
+  // 
+  healthPotionHeal = 30;
+  swordDamageIncrease = 20;
+  // 
+  maxEnemyMovePerTick = 5;
+  difficultyActiveEnemyPerLevel = 5; // +1 active enemy per 5 levels
   // -------------
   // stored values
   field = [];
   // 
-  enemyPositions = [];
+  enemies = [];
+  enemiesList = [];
   playerPosition = null;
   // 
   fieldRoot = null;
+  inventoryRoot = null;
   // 
   emptySpaces = [];
   // 
+  levelNumber = 1;
+  // 
+  playerHealth = this.maxPlayerHealth;
+  playerDamage = this.maxPlayerDamage;
 
 
   constructor() {
@@ -79,6 +154,16 @@ class Game {
 
   init() {
     this.Log('initialising game...', debugLvl.prod);
+    // 
+    this.playerHealth = this.maxPlayerHealth;
+    this.playerDamage = this.maxPlayerDamage;
+    this.levelNumber = 0;
+    // 
+    this.progressLevel();
+  }
+
+  progressLevel() {
+    this.levelNumber +=1;
     // 
     if(this.fitTilesToScreen)
       this.updateFieldSize();
@@ -96,6 +181,7 @@ class Game {
 
   captureRoot() {
     this.fieldRoot = document.getElementsByClassName(this.fieldClass)[0];
+    this.inventoryRoot = document.getElementsByClassName(this.inventoryClass)[0];
     this.Log(`got html element as root with class = '${this.fieldClass}'`);
   }
   // ----------------------------
@@ -115,29 +201,57 @@ class Game {
             this.moveTile(this.playerPosition, 1, 0);
             break;
         
-          case 'd':
+            case 'd':
             this.moveTile(this.playerPosition, 0, 1);
+            break;
+          
+            case ' ':
+            this.playerAttack();
+            break;
+          
+            case 'r':
+            this.progressLevel();
             break;
         
           default:
             break;
         }
         // 
+        this.enemyTick();
       this.renderScreen();
     }
     );
   }
   
   enemyTick() {
+    for (let i = 0; i < this.maxEnemyMovePerTick + ~~(this.levelNumber/this.difficultyActiveEnemyPerLevel); i++) {
+      if (i< this.enemiesList.length) {
+        let curEnemy = randomChoice(this.enemiesList); // TODO: one enemy could make several moves per enemyTick
+        let rndX = randomInRange(1, 4)-2;
+        let rndY = randomInRange(1, 4) - 2;
 
+        if ((curEnemy.position.minus(this.playerPosition)).length < 5) { // semi random direction if in activation radius
+          var dirVector = curEnemy.position.minus(this.playerPosition).normalizeRound();
+          rndX = dirVector.x;
+          rndY = dirVector.y;
+        }
+
+        let hasMoved = this.moveTile(curEnemy.position, rndX, rndY);
+        if (hasMoved) {
+          let curPosition = curEnemy.position;
+          this.enemies[curPosition.x][curPosition.y] = curEnemy; // add cur pos after move
+          this.enemies[curPosition.x-rndX][curPosition.y-rndY] = null; // remove previous pos after move
+        }
+      }
+    }
   }
 
   moveTile(originalPos, deltaX, deltaY) {
-    console.log(originalPos, deltaX, deltaY);
+    let ret = false;
     let currentTileSprite = this.lookUpTypes[this.field[originalPos.x][originalPos.y]];
     let nextTileSprite = this.lookUpTypes[this.field[originalPos.x + deltaX][originalPos.y + deltaY]];
     //
-    this.spriteLogicAdditional(currentTileSprite, nextTileSprite);
+    this.spriteLogicAdditional(currentTileSprite, nextTileSprite, originalPos, deltaX, deltaY);
     // 
     if (this.spriteLogicCanGo(currentTileSprite, nextTileSprite)) {
       this.field[originalPos.x][originalPos.y] = this.lookUpTypesInverse[this.spriteLogicStartLocation(currentTileSprite, nextTileSprite)];
@@ -146,19 +260,24 @@ class Game {
       originalPos.x = originalPos.x + deltaX;
       originalPos.y = originalPos.y + deltaY;
       // 
+      ret = true;
     }
+    return ret;
   }
   // game logic
   spriteLogicCanGo(originalSprite, nextSprite) { // ret true / false
     let ret = true;
 
-    if (nextSprite == this.tileWall) {
+    if (originalSprite == this.tileWall)
       ret = false;
-    }
-
-    if (nextSprite == this.tileEnemy) {
+    if (originalSprite == this.tileBase)
       ret = false;
-    }
+    if (nextSprite == this.tileWall)
+      ret = false;
+    if (nextSprite == this.tileEnemy)
+      ret = false;
+    if (nextSprite == this.tilePlayer)
+      ret = false;
 
     return ret;
   }
@@ -172,10 +291,10 @@ class Game {
     let ret = originalSprite;
     return ret;
   }
-  spriteLogicAdditional(originalSprite, nextSprite) { // run needed function
+  spriteLogicAdditional(originalSprite, nextSprite, originalPos, deltaX, deltaY) { // run needed function
     if (originalSprite == this.tilePlayer) {
       if (nextSprite == this.tileEnemy) {
-        this.decreaseHealth();
+        this.decreaseHealth(originalPos, deltaX, deltaY);
       }
       if (nextSprite == this.tileHealthPotion) {
         this.increaseHealth();
@@ -184,27 +303,77 @@ class Game {
         this.increaseDamage();
       }
     }
+
+    if (originalSprite == this.tileEnemy) {
+      if (nextSprite == this.tilePlayer) {
+        this.decreaseHealth(originalPos, deltaX, deltaY);
+      }
+    }
   }
   // ----
-  decreaseHealth() {
+  decreaseHealth(originalPos, deltaX, deltaY) {
     this.Log(`health decreased`);
-    
+    let damage = 0;
+
+    if (this.enemies[originalPos.x][originalPos.y] != null) {
+      damage = this.enemies[originalPos.x][originalPos.y].damage;
+    }
+    if (this.enemies[originalPos.x+deltaX][originalPos.y+deltaY] != null) {
+      damage = this.enemies[originalPos.x + deltaX][originalPos.y + deltaY].damage;
+    }
+
+    this.playerHealth -= damage;
+    // 
+    if (this.playerHealth < 0) {
+      alert("game over");
+      this.init();
+    }
   }
 
   increaseHealth() {
     this.Log(`health increased`);
+    this.playerHealth += this.healthPotionHeal;
+    if (this.playerHealth > this.maxPlayerHealth) {
+      this.playerHealth = this.maxPlayerHealth;
+    }
 
   }
 
   increaseDamage() {
     this.Log(`damage increased`);
+    this.playerDamage += this.swordDamageIncrease;
+  }
+  
+  playerAttack() {
+    this.Log(`playerAttack`);
+    let enemyTileIndex = this.lookUpTypesInverse[this.tileEnemy];
+    let emptyTileIndex = this.lookUpTypesInverse[this.tileBase];
 
+    for (let i = this.playerPosition.x - this.playerAttackRange; i < this.playerPosition.x + this.playerAttackRange+1; i++) {
+      for (let j = this.playerPosition.y - this.playerAttackRange; j < this.playerPosition.y + this.playerAttackRange+1; j++) {
+        if (this.field[i][j] === enemyTileIndex) {
+          // have found enemy
+          let enemy = this.enemies[i][j];
+          enemy.receaveDamage(this.playerDamage);
+
+          if (enemy.isDead) {
+          this.field[i][j] = emptyTileIndex;
+          this.enemies[i][j] = null;
+            
+          var index = this.enemiesList.indexOf(enemy);
+          if (index !== -1) {
+            this.enemiesList.splice(index, 1);
+          }
+          }
+        }
+      }
+    }
   }
 
   // ----------------------------
 
   updateFieldSize() {
-    // ?
+    // ?console
     var size = this.fieldRoot.getBoundingClientRect();
     this.fieldSizeX = size.width % this.tileSize / 2 + 1;
     this.fieldSizeY = size.height % this.tileSize / 2 + 1;
@@ -335,12 +504,17 @@ class Game {
 
   addEnemies() {
     this.Log(`adding enemies`);
-    this.enemyPositions = this.addItemsToField(this.tileEnemy, this.enemyCount);
-    this.LogObject(`enemy positions`, this.enemyPositions);
+    let enemyPositions = this.addItemsToField(this.tileEnemy, this.enemyCount);
+    this.LogObject(`enemy positions`, enemyPositions, debugLvl.full);
+    this.enemiesList = Enemy.processPositionsToEnemies(enemyPositions);
+    this.enemies = Enemy.createEnemyMatrix(this.fieldSizeX, this.fieldSizeY , this.enemiesList);
+    this.LogObject(`enemiesList`, this.enemiesList, debugLvl.full);
+    this.LogObject(`enemies`, this.enemies);
   }
   // ----------------------------
 
   renderScreen() {
+    // render field
     this.Log(`removig current fieldRoot childred`, debugLvl.full);
     this.fieldRoot.replaceChildren();
     // 
@@ -354,11 +528,40 @@ class Game {
           newElement.classList.add(this.lookUpTypes[val]);
         newElement.style['top'] = `${i * this.tileSize}px`;
         newElement.style['left'] = `${j * this.tileSize}px`;
+        // proces health bars
+        if ([this.tileEnemy, this.tilePlayer].includes(this.lookUpTypes[val])) {
+          var healthElement = document.createElement("div");
+          healthElement.classList.add("health");
+          let displayedHealth = this.playerHealth;
+          if (this.lookUpTypes[val] == this.tileEnemy) {
+            displayedHealth = this.enemies[i][j].health;
+          }
+          healthElement.style['width'] = `${this.tileSize * displayedHealth / 100}px`;
+          // 
+          newElement.appendChild(healthElement);
+        }
         // 
         this.fieldRoot.appendChild(newElement);
        }
       )
     });
+    // render inventory
+    // ?
+    this.inventoryRoot.replaceChildren();
+
+    let levelNumberElement = document.createElement("span");
+    levelNumberElement.innerText = `Уровень: ${this.levelNumber}`;
+
+    let playerHealthElement = document.createElement("span");
+    playerHealthElement.innerText = `здоровье: ${this.playerHealth}/${this.maxPlayerHealth}`;
+
+    let playerDamageElement = document.createElement("span");
+    playerDamageElement.innerText = `Урон: ${this.playerDamage}`;
+
+    this.inventoryRoot.appendChild(levelNumberElement);
+    this.inventoryRoot.appendChild(playerHealthElement);
+    this.inventoryRoot.appendChild(playerDamageElement);
+    // 
     this.Log(`finished rendering screen`, debugLvl.full);
   }
 
